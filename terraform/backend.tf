5c1ee223b51bd38331dbf57b0648842958d7cd5d
Mittu <47289707+Hydra2206@users.noreply.github.com>
2025-12-03 00:03:26 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,6 +4,6 @@ terraform {
     bucket       = "remote-backend-statefile-bucket"            #one thing to remember here we have to manually create a s3 bkt for Remote backend then it will use the bkt
     key          = "remote_backend/terraform.tfstate"
     region       = "ap-south-1"
-    use_lockfile = true
+    #use_lockfile = true
   }
 }
